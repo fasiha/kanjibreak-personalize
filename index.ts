74c9ff89b4f1308dd83f289f527c5f0ba370aab6
Ahmed Fasih <wuzzyview@gmail.com>
2018-11-20 20:34:48 -0500

    Basic TypeScript skeleton

---
+++ index.ts
@@ -0,0 +1,8 @@
+const KANJIBREAK_SQLITE_URL = "https://kanjibreak.glitch.me/api/exportdb";
+
+if (require.main === module) {
+  (async function main() {
+    console.log('whee');
+    console.log('whoo');
+  })();
+}
