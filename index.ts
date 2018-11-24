ef519080d9e8b8849c0106a3d06c3a9022346039
Ahmed Fasih <wuzzyview@gmail.com>
2018-11-23 23:54:53 -0500

    Process SQLite file because it can be fetched

--- index.ts
+++ index.ts
@@ -9,115 +9,104 @@ $ node index.js KANJIBREAK.CSV "制作の日本"
 to see the Markdown output.
 `;
 
-import {exists, readFile} from 'fs';
+import {exists, readFile, writeFile} from 'fs';
 import {promisify} from 'util';
+import fetch from 'node-fetch';
+import sqlite from 'sqlite3';
 
 const existsPromise = promisify(exists);
 const readFilePromise = promisify(readFile);
-function flatten1<T>(v: T[][]): T[] { return v.reduce((prev, curr) => prev.concat(curr), []); };
-
-const CSV_SEP = ',';
+const writeFilePromise = promisify(writeFile);
 
 type NodesEdges<T> = {
   nodes: Set<T>,
   edges: Map<T, Set<T>>
 };
-function allDescendents(deps: Map<string, string[][]>, kanji: string): NodesEdges<string> {
-  let nodes: Set<string> = new Set([kanji]);
-  let edges: Map<string, Set<string>> = new Map([]);
-  let parents = [kanji];
-  while (parents.length > 0) {
-    let cousins: string[] = [];
-    for (let kanji of parents) {
-      let hit = deps.get(kanji);
-      if (!hit) { continue; }
-      let kids = flatten1(hit).filter(x => x !== kanji); // no self-references
-      if (kids.length === 0) { continue; }
-      kids.forEach(kid => {
-        nodes.add(kid);
-        let tmp = edges.get(kanji) || new Set([]);
-        if (!tmp.has(kid)) { cousins.push(kid); } // don't walk down edges we've already traversed
-        edges.set(kanji, tmp.add(kid));
-      });
-    }
-    parents = cousins;
-  }
-  return {nodes, edges};
-}
 
 function graphToMarkdown(kanji: string, graph: NodesEdges<string>, kanjiPrinter: ((s: string) => string) = s => s,
                          visitedNodes: Set<string> = new Set([]), indent: number = 0): string {
-  let header = ' '.repeat(indent) + '- ' + kanjiPrinter(kanji);
+  const header = ' '.repeat(indent) + '- ' + kanjiPrinter(kanji);
   if (visitedNodes.has(kanji)) { return header + ' (repeat breakdown omitted)'; }
   visitedNodes.add(kanji);
 
   const hit = graph.edges.get(kanji);
   if (!hit) { return header; }
-  let ret =
-      header + '\n' + [...hit].map(k => graphToMarkdown(k, graph, kanjiPrinter, visitedNodes, indent + 2)).join('\n');
-  return ret;
+  const ret = [...hit]
+                  .filter(x => x !== kanji)
+                  .map(k => graphToMarkdown(k, graph, kanjiPrinter, visitedNodes, indent + 2))
+                  .join('\n');
+  return header + (ret ? '\n' + ret : '');
 }
 
-function dependencyTableToMap(dependencies: string[][]): Map<string, string[][]> {
-  let kanjiUserComponents: Map<string, Map<string, string[]>> = new Map([]);
-  for (let [kanji, user, component] of dependencies) {
-    if (kanjiUserComponents.has(kanji)) {
-      let userComponents = kanjiUserComponents.get(kanji);
-      if (!userComponents) { throw new Error('typescript pacification'); }
-      userComponents.set(user, (userComponents.get(user) || []).concat(component))
-    } else {
-      kanjiUserComponents.set(kanji, new Map([[user, [component]]]));
+function databaseToGraph(db: sqlite.Database): NodesEdges<string> {
+  let nodes: Set<string> = new Set([]);
+  let edges: Map<string, Set<string>> = new Map([]);
+  db.each("SELECT * FROM deps", (err, row) => {
+    if (err) { throw new Error('' + err); }
+    if (!(row.hasOwnProperty('target') && row.hasOwnProperty('dependency'))) {
+      throw new Error('malformed row in `deps`');
     }
-  }
+    nodes.add(row.target);
+    if (true || row.target !== row.dependency) { // don't allow self-references
+      nodes.add(row.dependency);
+      edges.set(row.target, (edges.get(row.target) || new Set([])).add(row.dependency));
+    }
+  });
+  return {nodes, edges};
+}
 
-  let kanjiComponents: Map<string, string[][]> = new Map([]);
-  for (let [kanji, userComponents] of kanjiUserComponents) {
-    kanjiComponents.set(kanji, Array.from(userComponents).map(([_, components]) => components));
+function databaseToMetadata(db: sqlite.Database) {
+  let valueIsPrimitive: Set<string> = new Set([]);
+  let valueIsKanji: Set<string> = new Set([]);
+  db.each("SELECT * FROM targets", (err, row) => {
+    if (err) { throw new Error('' + err); }
+    if (!(row.hasOwnProperty('target') && row.hasOwnProperty('primitive') && row.hasOwnProperty('kanji'))) {
+      throw new Error('malformed row in `targets`');
+    }
+    if (row.primitive) { valueIsPrimitive.add(row.target); }
+    if (row.kanji) { valueIsKanji.add(row.target); }
+  });
+  return {valueIsPrimitive, valueIsKanji};
+}
+
+async function verifyExistsOrDownload(sqliteFilepath = 'kanjibreak.sqlite3'): Promise<boolean> {
+  if (!existsPromise(sqliteFilepath)) {
+    await fetch('https://kanjibreak.glitch.me/api/exportdb')
+        .then(x => x.arrayBuffer())
+        .then(x => writeFilePromise(sqliteFilepath, Buffer.from(x)))
   }
-  return kanjiComponents;
+  return true;
 }
 
 if (require.main === module) {
   (async function main() {
     // Parse inputs
-    const [kanjibreakCsv, inputText]: (string|undefined)[] = process.argv.slice(2);
-    if (!kanjibreakCsv) {
+    const [KANJIBREAK_SQLITE, inputText]: (string|undefined)[] = process.argv.slice(2);
+    if (!KANJIBREAK_SQLITE) {
       console.log(USAGE);
       process.exit(1);
     }
-
-    // Slurp CSV file from KanjiBreak
-    if (!await existsPromise(kanjibreakCsv)) {
-      console.log('ERROR: cannot read input file, ' + kanjibreakCsv);
+    if (!await existsPromise(KANJIBREAK_SQLITE)) {
+      console.log('ERROR: cannot read input file, ' + KANJIBREAK_SQLITE);
       process.exit(1);
     }
-    const raw = await readFilePromise(kanjibreakCsv, 'utf8');
-    const sections = raw.trim().split('\n\n');
 
-    // Extract metadata table (allowed kanji, primitives, etc.)
-    const metadataSection = sections.find(s => s.startsWith('target,primitive,kanji'));
-    if (!metadataSection) {
-      console.error('ERROR: could not find metadata table in ' + kanjibreakCsv);
-      process.exit(1);
-      return;
-    }
-    const metadata: [string, boolean, boolean][] =
-        metadataSection.trim()
-            .split('\n')
-            .map(line => line.split(CSV_SEP))
-            .map(v => [v[0], !!(+v[1]), !!(+v[2])] as [string, boolean, boolean]);
-    const valueIsPrimitive: Set<string> = new Set(metadata.filter(([_, p]) => p).map(([k]) => k));
-    const valueIsKanji: Set<string> = new Set(metadata.filter(([_, _2, k]) => k).map(([k]) => k));
+    // Load database
+    let db = new sqlite.Database(KANJIBREAK_SQLITE, err => {
+      if (err) { throw new Error('failed to open ' + KANJIBREAK_SQLITE); }
+    });
 
-    // Extract dependency table
-    const dependencySection = sections.find(s => s.startsWith('target,user'));
-    if (!dependencySection) {
-      console.error('ERROR: could not find dependency table in ' + kanjibreakCsv);
-      process.exit(1);
-      return;
-    }
-    const dependencies = dependencySection.trim().split('\n').map(line => line.split(CSV_SEP));
-    let kanjiComponents: Map<string, string[][]> = dependencyTableToMap(dependencies);
+    // Extract metadata and dependencies tables
+    const {valueIsPrimitive, valueIsKanji} = databaseToMetadata(db);
+    const graph: NodesEdges<string> = databaseToGraph(db);
+
+    // Wait for all database queries to finish
+    await new Promise((resolve, reject) => {
+      db.close(e => {
+        if (e) { reject(e); }
+        resolve();
+      });
+    });
 
     // Load Kanji Kentei (kanken) data
     const kankenYearToKanjis: Map<number, string> = new Map(
@@ -137,7 +126,7 @@ if (require.main === module) {
     // Text given as an argument?
     if (inputText) {
       for (let k of new Set(inputText.match(hanRegexp))) {
-        console.log(graphToMarkdown(k, allDescendents(kanjiComponents, k), kankenPrinter));
+        console.log(graphToMarkdown(k, graph, kankenPrinter));
         console.log('');
       }
     } else {
@@ -148,7 +137,7 @@ if (require.main === module) {
         let kanjis = [...new Set(line.toString('utf8').match(hanRegexp))].filter(k => !seen.has(k));
         for (let k of kanjis) {
           seen.add(k);
-          console.log(graphToMarkdown(k, allDescendents(kanjiComponents, k), kankenPrinter));
+          console.log(graphToMarkdown(k, graph, kankenPrinter));
           console.log('');
         }
       });
@@ -156,7 +145,4 @@ if (require.main === module) {
   })();
 }
 
-module.exports = {
-  allDescendents,
-  graphToMarkdown
-};
+module.exports = {graphToMarkdown};
--- tests.js
+++ tests.js
@@ -2,16 +2,30 @@
 const test = require('tape');
 const mod = require('./index');
 
-let tmap = new Map([
-  ['q-fq', [['q', 'fq']]],
-  ['q', [['d']]],
-  ['d', [['s']]],
-  ['s', [['q'], ['s']]],
-  ['fq', [['q', 'f']]],
-]);
+let graph = {
+  edges: new Map([
+    ['q-fq', new Set(['q', 'fq'])],
+    ['q', new Set(['d'])],
+    ['d', new Set(['s'])],
+    [
+      's', new Set([
+        'q',
+        // 's' // ignore self loops
+      ])
+    ],
+    ['fq', new Set(['q', 'f'])],
+  ]),
+  nodes: new Set([
+    'q-fq',
+    'q',
+    'fq',
+    'd',
+    's',
+    'f',
+  ])
+};
 
 test('descender', t => {
-  let graph = mod.allDescendents(tmap, 'q-fq')
   t.equal(graph.nodes.size, 6, "found all nodes");
   t.ok(graph.edges.has('s'), "edge out of s");
   t.ok(graph.edges.get('s').has('q'), "edge between s and q (loop)");
@@ -20,7 +34,6 @@ test('descender', t => {
 })
 
 test('printer', t => {
-  let graph = mod.allDescendents(tmap, 'q-fq')
   let printed = mod.graphToMarkdown('q-fq', graph);
   t.comment(JSON.stringify(printed));
   t.ok(printed.indexOf('\n\n') < 0, "no repeated newlines");
